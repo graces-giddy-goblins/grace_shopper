43e830b1e3882b57b78a06ef70e59c64f2e2d731
Tae Hui Chang <jasmine878@yahoo.com>
2019-07-30 11:59:27 -0400

    Create model for All Items #17

---
+++ server/db/models/items.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Items = db.define('Items', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://vignette.wikia.nocookie.net/super-mario-kart-racing/images/5/5b/Item_Box_-_Mario_Kart_Wii.png/revision/latest?cb=20170331001434'
+  },
+  price: {
+    type: Sequelize.DECIMAL(3, 2),
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT
+  }
+})
+
+module.exports = Items
