7a3f4f09b46b1f11b0f3f95f178094ff17811911
sosterbind <34967988+sosterbind@users.noreply.github.com>
2019-07-31 17:06:05 -0400

    added a test but its buggy

---
+++ client/components/single-item.spec.js
@@ -0,0 +1,20 @@
+import {expect} from 'chai'
+import React from 'react'
+import enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import SingleItem from './single-item'
+
+// const adapter = new Adapter()
+// enzyme.configure({adapter})
+
+// describe('<SingleItem /> component', () => {
+//   let singleItem
+
+//   beforeEach('Create component', () => {
+//     singleItem = shallow(<SingleItem />)
+//   })
+
+//   it('has a `singleItem` field on state', () => {
+//     expect(singleItem.state().singleItem).to.be.an('object')
+//   })
+// })
